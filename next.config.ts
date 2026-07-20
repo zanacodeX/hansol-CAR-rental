8175be0e7a8c1b5d073606b4813bf2880c196783
zanacodeX <supuusandar@gmail.com>
2026-07-20 11:26:35 +0530

    Add allowed dev origin 192.168.130.124

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["bcryptjs"],
-  allowedDevOrigins: ["192.168.129.58"],
+  allowedDevOrigins: ["192.168.129.58", "192.168.130.124"],
 };
 
 export default nextConfig;
